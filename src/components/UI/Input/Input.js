76944c79bea9d184233989aa5898c35164e4d845
Parastoo Azariyan <pazariyan@gmail.com>
2022-11-19 00:28:11 +0330

    first commit

---
+++ src/components/UI/Input/Input.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+import './Input.css'
+
+const Input = (props) => {
+    let inputElement = null
+    switch (props.inputType) {
+        case 'input':
+                inputElement = <input className='input-element' 
+                {...props.elementConfig}
+                value={props.value}
+                onChange={props.change} />
+            break
+            default:
+                inputElement = <input className='input-element' 
+                {...props.elementConfig}
+                value={props.value}
+                onChange={props.change} />
+    }
+    
+    return <div className='input'>{inputElement}</div>
+}
+
+export default Input
